fac6fd4bf0e191c4f87cab32be39b8889db88cfa
Tinacp <tina.c.94@hotmail.com>
2023-04-16 23:27:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,80 @@ const cats = [
     }
 ];
 
+// Question 1:
+
+const cat = {
+    complain: function() {
+        console.log("Meow!")
+    }
+};
+
+cat.complain();
+
+
+// Question 2:
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+
+//Question 3:
+
+heading.style.fontSize = "2em";
+
+
+//Question 4:
+
+heading.classList.add("subheading");
+
+
+//Question 5:
+
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
+
+
+//Question 6:
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+
+//Question 7: 
+
+function catsList(list) {
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i].name)
+    }
+}
+
+catsList(cats);
+
+
+//Question 8: 
+//I struggled alot with this one, so I know that this is not right or finished.
+
+const catContainer = document.querySelector(".cat-container");
+
+function createCats(cats) {
+
+    let cat = "";
+
+
+    for(let i = 0; i < cats.length; i++) {
+        
+        cat = cat + "<div>" + cats[i] + "</div>";
+
+    }
+
+
+    return cat;
+}
+
+const finalHtml = createCats(cats);
+catContainer.innerHTML = finalHtml;
